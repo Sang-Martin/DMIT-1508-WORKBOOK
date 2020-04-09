019a297ade924ab047c8f8191058014321ca077d
Sang-Martin <59576275+Sang-Martin@users.noreply.github.com>
2020-04-08 23:15:22 -0600

    Update B - Stored Procedures - RAISERROR.sql

--- SPROCS/B - Stored Procedures - RAISERROR.sql
+++ SPROCS/B - Stored Procedures - RAISERROR.sql
@@ -1,5 +1,16 @@
 -- Stored Procedures (Sprocs)
 -- Validating Parameter Values
+--
+-- We can validate parameter values using IF/ELSE statements. An IF/ELSE statement is
+-- called a "flow-control" statement because it controls whether or not another statement
+-- (or statement block) will execute. The grammar of the IF/ELSE statement is as follows:
+-- IF (Conditional_Expression)
+--    Statement-or-Statement-Block -- TRUE side
+-- ELSE
+--    Statement-or-Statement-Block -- FALSE side
+--
+-- where the Conditional_Expression is some kind of expression that will result in a
+-- value of TRUE or FALSE.
 
 USE [A01-School]
 GO
@@ -35,6 +46,13 @@ AS
 RETURN
 GO
 
+-- Demo/Test my stored procedure
+EXEC AddClub 'CLUB', 'Central Library of Unused Books' -- Testing with "good" data
+-- Imagine that the sproc is called with !bad! data
+EXEC AddClub null, 'Gotcha'
+EXEC AddClub 'OOPS', null
+GO
+
 -- 1.b. Modify the AddClub procedure to ensure that the club name and id are actually supplied. Use the RAISERROR() function to report that this data is required.
 ALTER PROCEDURE AddClub
     -- Parameters here
@@ -42,19 +60,25 @@ ALTER PROCEDURE AddClub
     @ClubName   varchar(50)
 AS
     -- Body of procedure here
+    -- I validate by finding out if the data is poor. If so, then I report the problem
     IF @ClubId IS NULL OR @ClubName IS NULL
     BEGIN
         RAISERROR('Club ID and Name are required', 16, 1)
+        -- The 16 is the error number (we are basically constrained to a range of error numbers)
+        -- The 1 is the severity of the error
+        -- We can always use the 16, 1
     END
-    ELSE
+    ELSE -- Otherwise, I proceed to process the data
     BEGIN
         INSERT INTO Club(ClubId, ClubName)
         VALUES (@ClubId, @ClubName)
     END
 RETURN
 GO
-
-
+-- Imagine that the sproc is called with !bad! data
+EXEC AddClub null, 'Gotcha'
+EXEC AddClub 'OOPS', null
+GO
 -- 2. Make a stored procedure that will find a club based on the first two or more characters of the club's ID. Call the procedure "FindStudentClubs"
 -- The following stored procedure does the query, but without validation
 IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_TYPE = N'PROCEDURE' AND ROUTINE_NAME = 'FindStudentClubs')
@@ -73,8 +97,6 @@ GO
 EXEC FindStudentClubs NULL  -- What do you predict the result will be?
 EXEC FindStudentClubs ''    -- What do you predict the result will be?
 GO
-
-
 ALTER PROCEDURE FindStudentClubs
     @PartialID      varchar(10)
 AS
@@ -91,8 +113,6 @@ RETURN
 GO
 EXEC FindStudentClubs ''    -- What do you predict the result will be?
 GO
-
-
 -- The above change did not stop the select.
 -- To fix it, we need the ELSE side of the IF validation
 ALTER PROCEDURE FindStudentClubs -- Third time's the charm ;)
@@ -112,10 +132,8 @@ AS
     END
 RETURN
 GO
-
 EXEC FindStudentClubs ''    -- What do you predict the result will be?
 EXEC FindStudentClubs 'NA'  -- Should give good results with no errors.
-GO
 
 
 -- 3. Create a stored procedure that will change the mailing address for a student. Call it ChangeMailingAddress.
@@ -124,7 +142,6 @@ GO
 IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_TYPE = N'PROCEDURE' AND ROUTINE_NAME = 'ChangeMailingAddress')
     DROP PROCEDURE ChangeMailingAddress
 GO
-
 CREATE PROCEDURE ChangeMailingAddress
     -- Parameters here
     @StudentId  int,
@@ -150,7 +167,6 @@ AS
     END   -- ...B }
 RETURN
 GO
-
 -- 4. Create a stored procedure that allows us to make corrections to a student's name. It should take in the student ID and the corrected name (first/last) of the student. Call the stored procedure CorrectStudentName. Validate that the student exists before attempting to change the name.
 IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_TYPE = N'PROCEDURE' AND ROUTINE_NAME = 'CorrectStudentName')
     DROP PROCEDURE CorrectStudentName
@@ -163,6 +179,8 @@ AS
     IF @StudentId IS NULL OR @FirstName IS NULL OR @LastName IS NULL
         RAISERROR('All parameters are required.', 16, 1)
     ELSE IF NOT EXISTS (SELECT StudentID FROM Student WHERE StudentID = @StudentId)
+        -- The EXISTS() function will return true if there are 1 or more rows,
+        -- otherwise it will return false
         RAISERROR('That student id does not exist', 16, 1)
     ELSE
         UPDATE  Student
@@ -173,23 +191,88 @@ RETURN
 GO
 
 
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_TYPE = N'PROCEDURE' AND ROUTINE_NAME = 'CorrectStudentName')
-    DROP PROCEDURE CorrectStudentName
-GO
-
-
 
 -- 5. Create a stored procedure that will remove a student from a club. Call it RemoveFromClub.
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_TYPE = N'PROCEDURE' AND ROUTINE_NAME = 'RemoveFromClub')
+    DROP PROCEDURE RemoveFromClub
+GO
+CREATE PROCEDURE RemoveFromClub
+    @StudentId      int
+AS
+    IF @StudentId IS NULL
+        RAISERROR('StudentId is required', 16, 1)
+    ELSE
+        DELETE FROM Student
+        WHERE  StudentID = @StudentId
+RETURN
+GO
 
 
 -- Query-based Stored Procedures
 -- 6. Create a stored procedure that will display all the staff and their position in the school.
 --    Show the full name of the staff member and the description of their position.
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_TYPE = N'PROCEDURE' AND ROUTINE_NAME = 'ListStaff')
+    DROP PROCEDURE ListStaff
+GO
+CREATE PROCEDURE ListStaff
+AS
+    SELECT  FirstName + '  ' + LastName AS 'StaffMember', PositionDescription
+    FROM    Staff AS S
+        INNER JOIN Position AS P ON S.PositionID = p.PositionID
+RETURN
+GO
 
 -- 7. Display all the final course marks for a given student. Include the name and number of the course
 --    along with the student's mark.
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_TYPE = N'PROCEDURE' AND ROUTINE_NAME = 'GetStudentMarks')
+    DROP PROCEDURE GetStudentMarks
+GO
+CREATE PROCEDURE GetStudentMarks
+    @StudentId      int
+AS
+    IF @StudentId IS NULL
+        RAISERROR('StudentId is required', 16, 1)
+    ELSE
+        SELECT  C.CourseId, C.CourseName, Mark
+        FROM    Registration AS R
+            INNER JOIN Course AS C ON R.CourseId = C.CourseId
+        WHERE   Mark IS NOT NULL
+RETURN
+GO
 
 -- 8. Display the students that are enrolled in a given course on a given semester.
 --    Display the course name and the student's full name and mark.
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_TYPE = N'PROCEDURE' AND ROUTINE_NAME = 'SprocName')
+    DROP PROCEDURE SprocName
+GO
+CREATE PROCEDURE SprocName
+    @CourseNumber   char(7),
+    @Semester       char(5)
+AS
+    IF @CourseNumber IS NULL OR @Semester IS NULL
+        RAISERROR('Course Number and Semester are required', 16, 1)
+    ELSE
+        SELECT  CourseName,
+                FirstName + ' ' + LastName AS 'Student',
+                Mark
+        FROM    Student AS S
+            INNER JOIN Registration AS R
+                ON R.StudentID = S.StudentID
+            INNER JOIN Course AS C
+                ON R.CourseId = C.CourseId
+        WHERE   R.CourseId = @CourseNumber
+          AND   Semester = @Semester
+RETURN
+GO
 
 -- 9. The school is running out of money! Find out who still owes money for the courses they are enrolled in.
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_TYPE = N'PROCEDURE' AND ROUTINE_NAME = 'ListOutstandingBalances')
+    DROP PROCEDURE ListOutstandingBalances
+GO
+CREATE PROCEDURE ListOutstandingBalances
+AS
+    SELECT  FirstName + ' ' + LastName AS 'Student', BalanceOwing
+    FROM    Student
+    WHERE   BalanceOwing > 0
+RETURN
+GO
